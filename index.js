edee95b542a0a9a73e52bfceed69e13d95f26ea9
Md Salman Rahman Nayeem <129536577+Md-Salman-Rahman339@users.noreply.github.com>
2025-04-17 17:53:40 +0600

    uri update

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const verifyToken = (req, res, next) => {
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ieavp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ieavp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true`;
 
 const client = new MongoClient(uri, {
     serverApi: {
